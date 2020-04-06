d39c184d87216be29573b7777fc45363415b84ea
liitng3 <898260697@qq.com>
2020-04-06 14:24:00 +0800

    add

--- 10-timer-button/index.js
+++ 10-timer-button/index.js
@@ -1,17 +1,19 @@
 $(function(){
-  var $btn = $('input[type="button"]'),
-      num=10,
-      timer;
-  $btn.attr('disabled','disabled');
-  $btn.val('同意('+num+'s)');
-  timer=setInterval(function(){
+  var $btn = $('#btn');
+  var num=6;
+  var timer;
+  timer = setInterval(function(){
     num--;
-    if(num === 0){
-      $btn.val('同意');
-      $btn.removeAttr('disabled');
-      clearInterval(timer);
+    if(num===0){
+        clearInterval(timer);
+        $btn.val('同意');
+        $btn.removeAttr('disabled');
+        $btn.click(function(){
+          alert('好好学习，天天向上!')
+        })
     }else{
-      $btn.val('同意('+ num +'s)');
+        $btn.val('同意（'+num+'s)')
     }
-  },1000)
+  },1000) 
+
 })
