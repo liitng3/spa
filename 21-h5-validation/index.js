4e5985e506f628789f4fb5c9726b6c3aa6618fda
liitng3 <898260697@qq.com>
2020-04-08 21:48:33 +0800

    add

--- 21-h5-validation/index.js
+++ 21-h5-validation/index.js
@@ -1,65 +1,31 @@
-$(function(){
-  /**get dom elem */
-  var $width = $("#width"),
-      $height = $("#height"),
-      // $btnCal = $("#calculate"),
-      $perimeter = $("#perimeter"),
+$(function () {
+  //get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      // $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
       $form = $('form'),
-      $area = $("#area");
+      $area = $('#area');
 
-  /**calc button click event */
-  // $btnCal.click(clickCalc);
-  $form.submit(function(e){
-    e.preventDefault();
-    //get value
-    var w = Number($width .val()),
-        h = Number($height.val());
-    //calculate
-    var p = 2*FloatAdd(w,h),
-        a = FloatMul(w,h);
-    // output
-    $perimeter.val(p);
-    $area.val(a);
-  });
-  $(document).keydown(function(event){
-    if(event.keyCode == 13){
-      　event.preventDefault();
+  /*calculate button click event*/
+  $form.submit(function (e) {
+      e.preventDefault();
+
       //get value
-      var w = Number($width .val()),
+      var w = Number($width.val()),
           h = Number($height.val());
+
       //calculate
-      var p = 2*FloatAdd(w,h),
-          a = FloatMul(w,h);
-      // output
+      var p = roundFractional(2 * (w + h), 2),
+          a = roundFractional(w * h, 2);
+
+      //output
       $perimeter.val(p);
       $area.val(a);
-    }
   });
+
+  function roundFractional(x, n) {
+      return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  }
+
 });
-//加法运算精度处理
-function FloatAdd(arg1,arg2){  
-  var r1,r2,m; 
-  try{
-    r1=arg1.toString().split(".")[1].length
-  }catch(e){
-    r1=0;
-  }
-  try{
-    r2=arg2.toString().split(".")[1].length
-  }catch(e){
-    r2=0;
-  }
-  m=Math.pow(10,Math.max(r1,r2)); 
-  return (arg1*m+arg2*m)/m;
-}  
-//乘法运算精度处理
-function FloatMul(arg1,arg2){  
-  var m=0; 
-  try{
-    m+=arg1.toString().split(".")[1].length
-  }catch(e){}
-  try{
-    m+=arg2.toString().split(".")[1].length
-  }catch(e){}
-  return Number(arg1.toString().replace(".",""))*Number(arg2.toString().replace(".",""))/Math.pow(10,m);
-}
