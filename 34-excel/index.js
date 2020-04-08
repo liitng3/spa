6bda39379427d70de2dc3cf67de53a278e54505b
liitng3 <898260697@qq.com>
2020-04-08 22:55:17 +0800

    add

--- 34-excel/index.js
+++ 34-excel/index.js
@@ -1,34 +1,22 @@
-$(function () {
-  var data = [
-    ['login_name', '登录名', 'C(9)', 'N', 'Y', ''],
-    ['password', '密码', 'C(9)', 'N', 'N', ''],
-    ['chinese_name', '中文名', 'C(10)', 'N', 'N', ''],
-    ['english_name', '英文名', 'C(10)', 'Y', 'N', ''],
-    ['gender', '性别', 'C(1)', 'Y', 'N', ''],
-    ['address', '家庭住址', 'C(100)', 'Y', 'N', ''],
-    ['mobile', '联系电话', 'C(15)', 'Y', 'N', ''],
-    ['comment', '备注', 'C(100)', 'Y', 'N', '']
-  ];
+var data = [
+  ['Java', 1, "降", "-0.01%"],
+  ['C', 2, "升", "+2.44%"],
+  ['Python', 3, "升", "+1.44%"],
+  ['C++', 4, "降", "-2.58%"],
+  ['C#', 5, "升", "+2.07%"],
+  ['Visual Basic.NET', 6, "降", "-1.17%"],
+  ['JavaScript', 7, "降", "-0.85%"]
+],
+container = document.getElementById('example'),
+hot;
 
-  var container = $('#main');
-  
-  /* global Handsontable: true */
-  var hot = new Handsontable(container.get(0), {
-    data: data,
-    contextMenu: true,          // 启用快捷菜单
-    manualRowResize : true,     // 允许行拖动  
-    manualColumnResize : true,  // 允许列拖动 
-    rowHeaders: true,
-    // colHeaders: true,
-    colHeaders: [
-      '字段',
-      '说明',
-      '数据类型',
-      '允许为空',
-      '主键',
-      '备注'
-    ]
-  });
-
-  return hot;
+hot = new Handsontable(container, {
+  data: data,
+  startRows: 7,
+  startCols: 4,
+  colHeaders: true,
+  colWidths: 150,
+  colHeaders: ["语言名称","排名","升或降","变化幅度"],
+  className: "htCenter htMiddle",
+  licenseKey: 'non-commercial-and-evaluation'
 })
