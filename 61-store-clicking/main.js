9d22034e7548d87b6659b3130a62a20ba9a25507
liitng3 <898260697@qq.com>
2020-04-09 08:54:40 +0800

    add

--- 61-store-clicking/main.js
+++ 61-store-clicking/main.js
@@ -1,8 +1,8 @@
 $(function () {
   var $btn = $("#btn"),
     num = window.localStorage.getItem('num') || 0;
-  $btn.val("被点击了" + num + "次");
-  $btn.click(function () {
+    $btn.val("被点击了" + num + "次");
+    $btn.click(function () {
     num++;
     window.localStorage.setItem('num', num);
     $btn.val("被点击了" + num + "次");
